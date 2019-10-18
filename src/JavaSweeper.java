3fd7624bea5a6c88f36ccb2594284dcc2a800180
Vendoros <Good0908>
2019-10-19 02:47:39 +0800

    0.0.4 add res and modified JavaSweeper.class

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -3,6 +3,9 @@ import java.awt.*;
 
 public class JavaSweeper extends JFrame {
 
+    private final int IMAGE_SIZE = 50;
+    private final int COLS = 15;
+    private final int ROWS = 1;
     private JPanel panel;
 
     public static void main(String[] args) {
@@ -17,14 +20,16 @@ public class JavaSweeper extends JFrame {
 //https://youtu.be/YhHLlRqqKNI?list=PLt5E226f3KMEv6huOjYdpVKZIz1PSlvEA&t=94
 
     private void initPanel() {
-        panel = new JPanel();//инициализируем панель
+        panel = new JPanel()//инициализируем панель
         {
-//            @Override
-//            protected void paintComponent (Graphics g){
-//
-//        }
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(getImage("bomb"),0,0,this);
+                g.drawImage(getImage("num1"),IMAGE_SIZE,0,this);
+            }
         };
-        panel.setPreferredSize(new Dimension(500, 300));//задаем размеры панели
+        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE, ROWS * IMAGE_SIZE));//задаем размеры панели
         add(panel);//добавляем панель
     }
 
@@ -35,5 +40,10 @@ public class JavaSweeper extends JFrame {
         setResizable(false);//объявляем окно неизменяемым по размеру
         pack();//изменяет размер формы
         setLocationRelativeTo(null);//устанавливает форму по центру
+    }
+
+    private Image getImage(String name){
+         ImageIcon icon = new ImageIcon("res/img/"+ name+".png");
+         return icon.getImage();
     }
 }
