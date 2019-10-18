3e3d8744f6586c8db2c6da106b6debee5ea3a0d4
Vendoros <Good0908>
2019-10-18 18:40:58 +0800

    0.0.1 expand JavaSweeper.class

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,2 +1,24 @@
-public class JavaSweeper {
+import javax.swing.*;
+import java.awt.*;
+
+public class JavaSweeper extends JFrame {
+
+    private JPanel panel;
+
+    public static void main(String[] args) {
+        new JavaSweeper();
+    }
+
+    public JavaSweeper() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 300));
+        add(panel);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("MineSweeper");
+        setVisible(true);
+        setResizable(false);
+        pack();//изменяет размер формы
+        setLocationRelativeTo(null);//устанавливает форму по центру
+    }
+
 }
